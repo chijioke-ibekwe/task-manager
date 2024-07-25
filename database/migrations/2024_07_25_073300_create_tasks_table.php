211c66d3cbd2af4aff1004ee23935d4a37aeea27
Chijioke Ibekwe <ibekwe.chijioke18@gmail.com>
2024-07-25 17:34:04 +0100

    feat: install spatie query builder and create task table

--- database/migrations/2024_07_25_073300_create_tasks_table.php
+++ database/migrations/2024_07_25_073300_create_tasks_table.php
@@ -4,15 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->unique()->primary();
+            $table->string('description');
+            $table->string('status')->default('incomplete');
+            $table->date('due_date');
+            $table->uuid('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
